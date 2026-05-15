1c3e20ecb8406ed04f58b82fed2b55b659f1f102
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-05-15 22:25:39 +0300

    feat: new project - AutoFoam.UI

---
+++ AutoFoam.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AutoFoam.UI.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ AutoFoam.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AutoFoam.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
